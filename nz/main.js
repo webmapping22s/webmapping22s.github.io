3722ff5c077ea05dc72e82063dda179122c16094
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-04-07 15:44:55 +0200

    lat, lng, zoom Variablen eingeführt

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,15 @@
 /* Neuseelandreise Skript */
 // einzeiliger Kommentar
-var map = L.map('map').setView([-39.13, 175.65], 11);
+let lat = -39.13;
+let lng = 175.65;
+let zoom = 11;
+
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-39.13, 175.65]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Tongariro Nationalpark</h3>')
     .openPopup();
