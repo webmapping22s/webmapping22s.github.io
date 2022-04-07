d3c4cf1666c6d95677d138cb4c7ef0d5832fa66a
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-04-07 15:57:08 +0200

    array coords erstellt und abgefragt

--- nz/main.js
+++ nz/main.js
@@ -4,7 +4,13 @@ let lat = -39.13;
 let lng = 175.65;
 let zoom = 11;
 
-let map = L.map('map').setView([lat, lng], zoom);
+let coords = [-39.13, 175.65];
+console.log(coords);
+console.log(coords[0]);
+console.log(coords[1]);
+console.log(coords.length);
+
+let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
