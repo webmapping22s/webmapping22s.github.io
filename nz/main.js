b9f387a997fa68769098a0e7845253108345275a
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-04-07 17:07:55 +0200

    console.log geleert

--- nz/main.js
+++ nz/main.js
@@ -5,24 +5,24 @@ let lng = 175.65;
 let zoom = 11;
 
 let coords = [-39.13, 175.65];
-console.log(coords);
-console.log(coords[0]);
-console.log(coords[1]);
-console.log(coords.length);
+//console.log(coords);
+//console.log(coords[0]);
+//console.log(coords[1]);
+//console.log(coords.length);
 
-console.log("text");
-console.log('text');
-console.log('id="map"');
-console.log(`latitude = ${lat}`);
+//console.log("text");
+//console.log('text');
+//console.log('id="map"');
+//console.log(`latitude = ${lat}`);
 
-console.log(ETAPPEN);
-console.log(ETAPPEN[0]);
-console.log(ETAPPEN[0].nr);
-console.log(ETAPPEN[0].github);
-console.log(ETAPPEN[0].titel);
-console.log(ETAPPEN[0].wikipedia);
-console.log(ETAPPEN[0].lat);
-console.log(ETAPPEN[0].lng);
+//console.log(ETAPPEN);
+//console.log(ETAPPEN[0]);
+//console.log(ETAPPEN[0].nr);
+//console.log(ETAPPEN[0].github);
+//console.log(ETAPPEN[0].titel);
+//console.log(ETAPPEN[0].wikipedia);
+//console.log(ETAPPEN[0].lat);
+//console.log(ETAPPEN[0].lng);
 
 
 let popup = `
